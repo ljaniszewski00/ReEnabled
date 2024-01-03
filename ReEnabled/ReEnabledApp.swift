c87e1cd6b78cc73d22308185d18c95a316ea3ba9
Łukasz Janiszewski <ljaniszewski00@gmail.com>
2024-01-03 19:39:37 +0100

    Initial Commit

---
+++ ReEnabled/ReEnabledApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReEnabledApp.swift
+//  ReEnabled
+//
+//  Created by Łukasz Janiszewski on 03/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ReEnabledApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
